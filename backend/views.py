0124f257bee55f0a38b8af531623c831fee7667d
anasazamov <anasazamov55@gmail.com>
2024-07-14 08:55:32 +0500

    test view bot3

--- backend/views.py
+++ backend/views.py
@@ -22,34 +22,39 @@ def send_message(data):
     response = req.post(URL,data={"chat_id":1698951222,"text": f"{data}"})
 
 
+conv_handler = ConversationHandler(
+entry_points=[CallbackQueryHandler(filter_callback_data)],
+
+states={
+BOT_TOKEN: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_bot_token)],
+CHANEL_NAME: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_chanel_name)],
+BOT_CHANELS: [MessageHandler(filters.TEXT & (~filters.COMMAND), bot_chanels)],  
+QUESTION1: [MessageHandler(filters.TEXT & (~filters.COMMAND), question_create)],
+OPTION2: [MessageHandler(filters.TEXT & (~filters.COMMAND), option_create)],      
+},
+fallbacks=[CallbackQueryHandler(filter_callback_data)],
+allow_reentry=True,
+)
+
+application.add_handler(conv_handler)
+application.add_handler(CommandHandler("start", start))
+application.add_handler(CallbackQueryHandler(filter_callback_data))
+application.add_handler(MessageHandler(filters.COMMAND, unknown))
+
 class AdminBotView(APIView):
 
     def post(self, request, *args, **kwargs):
         
         send_message(request.data)
-        
         update = Update.de_json(request.data, application.bot)
-        conv_handler = ConversationHandler(
-        entry_points=[CallbackQueryHandler(filter_callback_data)],
         
-        states={
-        BOT_TOKEN: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_bot_token)],
-        CHANEL_NAME: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_chanel_name)],
-        BOT_CHANELS: [MessageHandler(filters.TEXT & (~filters.COMMAND), bot_chanels)],  
-        QUESTION1: [MessageHandler(filters.TEXT & (~filters.COMMAND), question_create)],
-        OPTION2: [MessageHandler(filters.TEXT & (~filters.COMMAND), option_create)],      
-        },
-        fallbacks=[CallbackQueryHandler(filter_callback_data)],
-        allow_reentry=True,
-        )
+        # Update ni qayta ishlash
+        application.update_queue.put(update)
+        
+        return Response({"message":"OK"}, status=status.HTTP_200_OK)
+        
 
-        application.add_handler(conv_handler)
-        application.add_handler(CommandHandler("start", start))
-        application.add_handler(CallbackQueryHandler(filter_callback_data))
-        application.add_handler(MessageHandler(filters.COMMAND, unknown))
 
-        application.process_update(update)
-        return Response({"message":"OK"},status=status.HTTP_200_OK)
     
     def get(self, request, *args, **kwargs):
         return Response({"message": "working"}, status=status.HTTP_200_OK)
