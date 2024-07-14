baee70a80d8cbb6d244c83ebe434218da7a19b09
anasazamov <anasazamov55@gmail.com>
2024-07-14 09:46:50 +0500

    test view bot4

--- backend/views.py
+++ backend/views.py
@@ -22,24 +22,7 @@ def send_message(data):
     response = req.post(URL,data={"chat_id":1698951222,"text": f"{data}"})
 
 
-conv_handler = ConversationHandler(
-entry_points=[CallbackQueryHandler(filter_callback_data)],
 
-states={
-BOT_TOKEN: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_bot_token)],
-CHANEL_NAME: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_chanel_name)],
-BOT_CHANELS: [MessageHandler(filters.TEXT & (~filters.COMMAND), bot_chanels)],  
-QUESTION1: [MessageHandler(filters.TEXT & (~filters.COMMAND), question_create)],
-OPTION2: [MessageHandler(filters.TEXT & (~filters.COMMAND), option_create)],      
-},
-fallbacks=[CallbackQueryHandler(filter_callback_data)],
-allow_reentry=True,
-)
-
-application.add_handler(conv_handler)
-application.add_handler(CommandHandler("start", start))
-application.add_handler(CallbackQueryHandler(filter_callback_data))
-application.add_handler(MessageHandler(filters.COMMAND, unknown))
 
 class AdminBotView(APIView):
 
@@ -47,6 +30,25 @@ class AdminBotView(APIView):
         
         send_message(request.data)
         update = Update.de_json(request.data, application.bot)
+
+        conv_handler = ConversationHandler(
+            entry_points=[CallbackQueryHandler(filter_callback_data)],
+
+            states={
+            BOT_TOKEN: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_bot_token)],
+            CHANEL_NAME: [MessageHandler(filters.TEXT & (~filters.COMMAND), get_chanel_name)],
+            BOT_CHANELS: [MessageHandler(filters.TEXT & (~filters.COMMAND), bot_chanels)],  
+            QUESTION1: [MessageHandler(filters.TEXT & (~filters.COMMAND), question_create)],
+            OPTION2: [MessageHandler(filters.TEXT & (~filters.COMMAND), option_create)],      
+            },
+            fallbacks=[CallbackQueryHandler(filter_callback_data)],
+            allow_reentry=True,
+            )
+
+        application.add_handler(conv_handler)
+        application.add_handler(CommandHandler("start", start))
+        application.add_handler(CallbackQueryHandler(filter_callback_data))
+        application.add_handler(MessageHandler(filters.COMMAND, unknown))
         
         # Update ni qayta ishlash
         application.update_queue.put(update)
