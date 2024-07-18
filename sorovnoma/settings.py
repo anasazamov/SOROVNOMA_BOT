5888d45f3731eb37e0e2e8591ecf80d5ffe2048a
anasazamov <anasazamov55@gmail.com>
2024-07-19 00:20:02 +0500

    change config

--- sorovnoma/settings.py
+++ sorovnoma/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'sorovnoma.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default='postgresql://postgres:postgres@localhost:5432/sorovnoma-bot',
         conn_max_age=600
     )
 }
